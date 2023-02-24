e88a3e0a6fd1b30022a0901d5f6b5eb48f6d0298
SGomezPalacios <sofiagp45@gmail.com>
2023-02-24 16:39:44 -0300

    main.js

---
+++ js/main.js
@@ -0,0 +1,34 @@
+console.log(data)
+
+const eventos = data.events;
+console.log(eventos);
+
+let eventCards= [];
+
+function createEvents(){
+    for(let event of eventos){
+        let cards = 
+            `<div class="card card-details">
+                <img src="${event.image}" class="card-img-top img-category" alt="...">
+                <div class="card-body">
+                <h4 class="card-title">${event.name}</h4>
+                <p class="card-text">${event.description}</p>
+                <p class="card-text">${event.date}</p>
+                <p class="card-text">${event.price}</p>
+                </div>
+                <div class="card-body">
+                    <button class="button-font" type="button" name="buy" id="buy">BUY</button>
+                </div>
+            </div>`
+            
+        eventCards.push(cards)
+    }
+}
+
+function printEvents (){
+    let cards = document.getElementById('cardEvents');
+    cards.innerHTML= eventCards.join('')
+}
+
+createEvents();
+printEvents();
