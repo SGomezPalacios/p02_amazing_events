e504859d9d53b7a355a3887424b0681f401b086c
SGomezPalacios <sofiagp45@gmail.com>
2023-03-14 15:24:45 -0300

    correciones task3 en details.js

--- js/details.js
+++ js/details.js
@@ -1,24 +1,32 @@
 console.log(data)
-
+let dates = data.events;
 let query = location.search;
-
 let params = new URLSearchParams(query);
 console.log(params)
 let id = params.get("_id");
 
-let cardEvents= data.events.find((evento) => evento._id == id)
-console.log(cardEvents)
+function createDetail(cardEvents){ 
+    return`
+    <div class="card card-details">
+        <img src="${cardEvents.image}" class="card-img-top img-category" alt="...">
+        <div class="card-body">
+            <h4 class="card-title">${cardEvents.name}</h4>
+            <p class="card-text">${cardEvents.description}</p>
+            <p class="card-text">Date: ${cardEvents.date}</p>
+            <p class="card-text">Price: $ ${cardEvents.price}</p>                    
+        </div>
+        <div class="card-body">
+            <button class="button-font" type="button" name="buy" id="buy">BUY</button>
+        </div>
+    </div>
+    `
+}
 
-let detailsCard = document.getElementById('cardDetails')
-detailsCard.innerHTML= `<div class="card card-details">
-    <img src="${cardEvents.image}" class="card-img-top img-category" alt="...">
-    <div class="card-body">
-    <h4 class="card-title">${cardEvents.name}</h4>
-    <p class="card-text">${cardEvents.description}</p>
-    <p class="card-text">Date: ${cardEvents.date}</p>
-    <p class="card-text">Price: $ ${cardEvents.price}</p>                    
-    </div>
-    <div class="card-body">
-        <button class="button-font" type="button" name="buy" id="buy">BUY</button>
-    </div>
-    </div>`;
+function printDetail(id, cardEvents, array_data){
+    let detailsCard = document.querySelector(id)
+    let cardsEvents = array_data.find(each => each._id == cardEvents)
+    let detail = createDetail(cardsEvents)
+    detailsCard.innerHTML = detail
+}
+
+printDetail(`#cardDetails`, id, dates)
